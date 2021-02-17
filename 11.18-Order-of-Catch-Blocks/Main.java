8ac69cbcaac3424f1766d62b9dcfca07d4761379
Dani Hooven <55116910+danihooven@users.noreply.github.com>
2021-02-17 17:23:46 -0500

    Add files via upload

---
+++ 11.18-Order-of-Catch-Blocks/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+  public static void main(String[] args) {
+    try {
+    } catch(Exception e) {
+      System.out.println(e);
+    } catch(NumberFormatException nfe) { 
+      System.out.println(nfe);
+    }
+  }
+}
